bc9f2acc9f8243a4c85113da835ac139adc861c2
kmiracles <miraclekambai2@gmail.com>
2026-05-09 10:24:39 +0100

    first API

---
+++ server/app.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const port = process.env.PORT || 5000;
+const app = express();
+
+const API_URL = 'https://v6.exchangerate-api.com/v6/';
+const API_KEY = process.env.API_KEY;
+
+const apiLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+});
+
+
+//! Middleware
+app.use(express.json());
+app.use(apiLimiter);
+
+//! conversation routes
+app.post('/api/convert', async (req, res) => {
+    try {
+       // get the user data
+        const  { from, to, amount } = req.body;
+    console.log({ from, to, amount });
+    // construct the api
+    const url = `${API_URL}/${API_KEY}/pair/${from}/${to}/${amount}`;
+      const response = await axios.get(url);
+      console.log(response);
+      
+    } catch (error) {}
+});
+
+
+//! start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
